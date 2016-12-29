0b97ae4da938359a5633a02aeeb1d71e808d345f
PineWong <pinewong@163.com>
2016-12-30 00:03:56 +0800

    Finish the following list.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -47,21 +48,38 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def following
+    @title = 'Following'
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    
+    render 'show_follow'
+  end
 
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-    # 前置过滤器
 
-    # 确保是正确的用户
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-    # 确保是管理员
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+  # 前置过滤器
+
+  # 确保是正确的用户
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+  # 确保是管理员
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
